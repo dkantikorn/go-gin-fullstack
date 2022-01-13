d262a3a16f6d5ed0c9ec4b892aec272b32023f34
DK_Antikorn <sarawutt.b@pakgon.com>
2022-01-13 18:42:12 +0700

    frontend complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,43 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Routes from './Route';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux"
+import './index.css'
+import store from './store/index'
+import setAuthorizationToken  from './authorization/authorization';
+import { LOGIN_SUCCESS } from './store/modules/auth/authTypes';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//when the page reloads, the auth user is still set
+if (localStorage.token){
+  setAuthorizationToken(localStorage.token) 
+  let userData = localStorage.getItem('user_data') == null ? null : JSON.parse(localStorage.getItem('user_data'))
+  store.dispatch({ type: LOGIN_SUCCESS, payload: userData}) //provided he has a valid token 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+
+ReactDOM.render(<Provider store={store}><Routes /></Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
